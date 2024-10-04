ffe63d7b2684b74af6d9a9edb43717fc82d10861
Maxim Efimov <efimov.top.academy@gmail.com>
2024-10-04 19:26:19 +0300

    Добавьте файлы проекта.

---
+++ WpfWelcomeApp/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWelcomeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //grid.Background = new SolidColorBrush(Colors.Red);
+
+            //Button button = new Button();
+            //button.Content = "Click me";
+            //button.Width = 200;
+            //button.Height = 100;
+
+            //grid.Children.Add(button);
+        }
+    }
+}
